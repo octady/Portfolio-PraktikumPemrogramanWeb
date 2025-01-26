b88ea8dc029efd75a890ccd0f4ce92e08eb3926f
octady <octadamayanti23@gmail.com>
2025-01-26 14:11:48 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login Form</title>
+        <link rel="stylesheet" href="style.css">
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    </head>
+    <body>
+         <div class="wrapper">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <h1>Login</h1>
+            <div class="input-box">
+                <input type="text" name="username" placeholder="Username" required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" name="password" placeholder="Password" required>
+                <i class='bx bxs-lock-alt'></i>
+            </div>
+            <div class="remember">
+                <label><input type="checkbox" name="remember">Remember me</label>
+            </div>
+                <input type="submit" class="btn" value="Login" formaction="portfolio.html">
+        </form>
+    </body>
